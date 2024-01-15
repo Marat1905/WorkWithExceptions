fdddcb74415c07a31798cd833b47b15e29a3c4ea
Marat <milya010790@mail.ru>
2024-01-15 15:55:51 +0500

    Переопределили свойство Message

--- WorkWithExceptions.Common/Exceptions/CustomException.cs
+++ WorkWithExceptions.Common/Exceptions/CustomException.cs
@@ -5,5 +5,9 @@
         public CustomException() { }
 
         public CustomException(string? message) : base(message) { }
+
+        public CustomException(string? message, Exception? innerException) : base(message, innerException) { }
+
+        public override string Message => $"{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")} - {base.Message}" ;
     }
 }
